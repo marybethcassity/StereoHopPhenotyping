8996f3e6e483e37e96f05c321bc60f59d04c6623
mec0104 <mec0104@auburn.edu>
2023-03-23 14:21:30 -0500

    Changed .py file names

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import open3d as o3d
+import os
+
+from create_pointclouds import create_pointclouds
+from create_ply import create_ply
+
+stereo = cv2.StereoSGBM_create(minDisparity= min_disp,
+    numDisparities = num_disp,
+    blockSize = 3,
+    uniquenessRatio = 5,
+    speckleWindowSize = 5,
+    speckleRange = 5,
+    disp12MaxDiff = 1,
+    P1 = 8*3*win_size**2,
+    P2 =32*3*win_size**2)  
+
+fx = 1054.7600
+fy = 1054.4700
+cx = 956.0000
+cy = 570.3380
+f = fx + fy / 2
+Tx = 0.1197760
+
+win_size = 5
+min_disp = 25
+max_disp = 281
+num_disp = max_disp - min_disp 
+
+def main():
+
